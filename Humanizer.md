b115aebc8340cc69a774de7d1807620ab1aad13a
Maria Naggaga <maria.naggaga@live.ca>
2019-04-24 16:21:06 -0400

    Testing Humanizer

--- Humanizer.md
+++ Humanizer.md
@@ -1,8 +1,15 @@
-# AntMan is having a party 
+# Ant Man is having a party 
 ![](.\Images\AntMan.gif)
 
 **No Spoliers**
 
-Hey Friends, I am know we are saving the world but, I thought you could come hang. 
-```csharp --source-file .\test_humanizer\Program.cs --project .\test_humanizer\Program.csproj --region run
+Hey Friends, I know we are saving the world but, I thought you might want to my house warming. 
+Let me know what you would like and who's in :) .
+```csharp --source-file .\test_humanizer\Program.cs --project .\test_humanizer\test_humanizer.csproj --region run
+```
+```csharp --source-file .\test_humanizer\Program.cs --project .\test_humanizer\test_humanizer.csproj --region run1
+```
+```csharp --source-file .\test_humanizer\Program.cs --project .\test_humanizer\test_humanizer.csproj --region run2
+```
+```csharp --source-file .\test_humanizer\Program.cs --project .\test_humanizer\test_humanizer.csproj --region run3
 ```
--- test_humanizer/Program.cs
+++ test_humanizer/Program.cs
@@ -22,26 +22,38 @@ namespace test_humanizer
                 case "run2":
                     Run2();
                     break;
+                case "run3":
+                    Run2();
+                    break;   
             }
             
         }
         public static void Run()
         {
             #region run
-            string singular = "geese".Singularize();
-            Console.WriteLine("Thor " + singular);
+            string singular = "meats".Singularize();
+            Console.WriteLine("Thor: {0}", singular);
             #endregion
         }
         public static void Run1()
         {
             #region run1
-        
+            string roman  = 2010.ToRoman();
+            Console.WriteLine("Tony Stark:{0}",roman);
             #endregion
         }
         public static void Run2()
         {
             #region run2
-        
+              string ordinalword = 21.ToOrdinalWords();
+              Console.WriteLine("Black Widow:Maybe {0} of June after we deal with Thanos?",ordinalword);
+            #endregion
+        }
+        public static void Run2()
+        {
+            #region run3
+              string ordinalword = 21.ToOrdinalWords();
+              Console.WriteLine("Ant Man:",ordinalword);
             #endregion
         }
        
